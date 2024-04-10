cac1e5a5314a027c3871e85076975f98d72d09fe
Jonathan Hudson <jh+github@daria.co.uk>
2024-04-10 19:51:31 +0100

    widen cli file test

--- mission.go
+++ mission.go
@@ -868,12 +868,7 @@ func handle_mission_data(dat []byte, path string) (string, *MultiMission) {
 	case bytes.Contains(dat[0:100], []byte(`"fileType": "Plan"`)):
 		mtype = "qgc-json"
 		m = process_qgc(dat, mtype)
-	case bytes.HasPrefix(dat, []byte("# wp")),
-		bytes.HasPrefix(dat, []byte("#wp")),
-		bytes.HasPrefix(dat, []byte("wp 0")),
-		bytes.HasPrefix(dat, []byte("# dump")),
-		bytes.HasPrefix(dat, []byte("# version")),
-		bytes.HasPrefix(dat, []byte("wp ")):
+	case bytes.HasPrefix(dat, []byte("# ")):
 		mtype = "inav cli"
 		m = read_inav_cli(dat)
 	default:
